b319106730a40f83d06299afa84b9252efed0c75
orthagonal <orthagonal1@gmail.com>
2017-09-05 22:12:01 -0500

    use returnCmd

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
 'use strict';
 const TaskKitTask = require('taskkit-task');
 const runShell = require('runshell');
+const path = require('path');
 
 class ShellTask extends TaskKitTask {
   // returns the module to load when running in a separate process:
   get classModule() {
     return path.join(__dirname, 'index.js');
-  }  
+  }
   execute(done) {
-    runShell(this.options.command, {
+    const cmd = runShell(this.options.command, {
       args: this.options.args,
-      log: true,
+      log: false,
+      returnCmd: true,
       timeout: this.options.timeout,
       env: this.options.env
-    }, (err, results) => {
+    });
+    cmd.stderr.on('data', (data) => {
+      this.log(data.toString());
       if (!this.options.continue) {
-        return done(err);
+        return done(new Error(data.toString()));
       }
     });
-    if (this.options.continue === true) {
-      return done();
-    }
+    cmd.stdout.on('data', (data) => {
+      this.log(data.toString());
+      if (this.options.continue === true) {
+        return done();
+      }
+    });
   }
 }
 
